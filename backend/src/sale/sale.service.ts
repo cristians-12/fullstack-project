7b216e961d8f5059d2ae31982c73cf394188bc7c
cristians-12 <cristian.sanchez.casd@gmail.com>
2025-05-16 00:20:50 -0500

    feat: getAll sales service

--- backend/src/sale/sale.service.ts
+++ backend/src/sale/sale.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Sale } from './model/sale.model';
 
 @Injectable()
-export class SaleService {}
+export class SaleService {
+    constructor(
+        @InjectRepository(Sale)
+        private readonly saleRepository: Repository<Sale>,
+    ) { }
+
+    async getAll(): Promise<Sale[]> {
+        return await this.saleRepository.find();
+    }
+}
