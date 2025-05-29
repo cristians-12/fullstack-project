a452e30f8b61f717e63f0febf2138436c6878ebb
cristians-12 <cristian.sanchez.casd@gmail.com>
2025-05-28 23:37:09 -0500

    feat: guards on menu

--- backend/src/menu/menu.controller.ts
+++ backend/src/menu/menu.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Menu } from './model/menu.model';
 import { CreateItemMenuRequestDTO } from './dto/create-item-menu.request';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('menu')
 export class MenuController {
     constructor(private readonly menuService: MenuService) { }
 
+    @UseGuards(AuthGuard)
     @Post()
     @UseInterceptors(FileInterceptor('image'))
     async create(
@@ -17,6 +19,7 @@ export class MenuController {
         return this.menuService.create(body, image);
     }
 
+    @UseGuards(AuthGuard)
     @Get()
     async get_all(): Promise<Menu[]> {
         return this.menuService.get_all();
